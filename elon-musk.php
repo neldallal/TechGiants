9e8c00c430753b4b29212df687200e19bdeddf28
Nadine Eldallal <86070676+neldallal@users.noreply.github.com>
2022-01-17 15:14:40 -0500

    Add files via upload

---
+++ elon-musk.php
@@ -0,0 +1,207 @@
+    <?php 
+    $customCSS = "";
+    include "inc/html-top.php"; 
+    ?>
+
+    <header class="page-header">
+        <a href="index.php"><h1 class="page-h1">Tech Tycoons</h1></a>
+        <div>Legends that shaped the industry</div>
+        <?php include "inc/nav.php"; ?>
+    </header>
+
+        <main>
+            <div class="container">
+            <header class="content-header">
+                <h2>Elon Musk</h2>
+                <div>The most interesting man in the world</div>
+            </header>
+
+            <section class="lead">
+            <h3>Brief Outlook</h3>
+
+            <figure class="elon-image">
+                <img class="flexible" src="images/elonmusk.jpg" alt="Image failed to load.">
+            </figure>
+                <p>
+                    Elon Reeve Musk FRS (/ˈiːlɒn/ EE-lon; born June 28, 1971) is a business magnate, industrial designer, and engineer. He is the founder, CEO, CTO, and chief designer of SpaceX; early investor, CEO, and product architect of Tesla, Inc.; founder of The Boring Company; co-founder of Neuralink; and co-founder and initial co-chairman of OpenAI. A <strong>centibillionaire</strong>, Musk is one of the richest people in the world.
+                </p>
+                <p>
+                   Musk was born to a Canadian mother and South African father and raised in Pretoria, South Africa. He briefly attended the University of Pretoria before moving to Canada aged 17 to attend Queen's University. He transferred to the University of Pennsylvania two years later, where he received dual bachelor's degrees in economics and physics. He moved to California in 1995 to attend Stanford University but decided instead to pursue a business career, co-founding web software company Zip2 with his brother Kimbal. The startup was acquired by Compaq for $307 million in 1999. Musk co-founded online bank X.com that same year, which merged with Confinity in 2000 to form the company PayPal and was subsequently bought by eBay in 2002 for $1.5 billion. 
+                </p>
+                <p>
+                    In 2002, Musk founded SpaceX, an aerospace manufacturer and space transport services company, of which he is CEO, CTO, and lead designer. In 2004, he joined electric vehicle manufacturer Tesla Motors, Inc. (now Tesla, Inc.) as chairman and product architect, becoming its CEO in 2008. In 2006, he helped create SolarCity, a solar energy services company and current Tesla subsidiary. In 2015, he co-founded OpenAI, a nonprofit research company that promotes friendly artificial intelligence. In 2016, he co-founded Neuralink, a neurotechnology company focused on developing brain–computer interfaces, and founded The Boring Company, a tunnel construction company. Musk has also proposed the Hyperloop, a high-speed vactrain transportation system.
+                </p>
+                <p>
+                    Musk has been the subject of criticism due to unorthodox or unscientific stances and highly publicized controversies. In 2018, he was sued for defamation by a diver who advised in the Tham Luang cave rescue; a California jury ruled in favor of Musk. Also in 2018, the US Securities and Exchange Commission (SEC) sued him for falsely tweeting that he had secured funding for a private takeover of Tesla. He settled with the SEC, temporarily stepping down from his chairmanship and accepting limitations on his Twitter usage. Musk has received criticism for his views on such matters as artificial intelligence and the COVID-19 pandemic.
+                </p>
+
+
+        </section>
+
+        <article>
+            <h3>Early Life</h3>
+
+            <div class=grid1-elon>
+            <section class="item1-elon">
+
+                <h4 class="elon-h4">Childhood</h4>
+                <p>
+                    Elon Reeve Musk was born on June 28, 1971, in Pretoria, Transvaal, South Africa. His mother is Maye Musk (née Haldeman), a model and dietitian born in Saskatchewan, Canada, but raised in South Africa. His father is Errol Musk, a South African electromechanical engineer, pilot, sailor, consultant, and property developer. He has a younger brother, Kimbal (born 1972), and a younger sister, Tosca (born 1974). His maternal grandfather, Joshua Haldeman, was an American-born Canadian. His paternal grandmother had British and Pennsylvania Dutch ancestry.
+                </p>
+                <p>
+                    After his parents divorced in 1980, Musk lived mostly with his father in the suburbs of Pretoria, a choice he made two years after the divorce and subsequently regretted. Musk has become estranged from his father, whom he has described as "a terrible human being... Almost every evil thing you could possibly think of, he has done." He has a half-sister and a half-brother on his father's side.
+                </p>
+                <p>
+                    At the age of 10, he developed an interest in computing while using the Commodore VIC-20. He learned computer programming using a manual and, by age 12, sold the code of a BASIC-based video game he created called <em>Blastar</em> to <em>PC and Office Technology</em> magazine for approximately $500. An awkward and introverted child, Musk was bullied throughout his childhood and was once hospitalized after a group of boys threw him down a flight of stairs. He attended Waterkloof House Preparatory School and Bryanston High School before graduating from Pretoria Boys High School.
+                </p>
+
+            </section>
+
+            <section class="item2-elon">
+                <h4 class="elon-h4">Education</h4>
+                
+                <p>
+                    Aware it would be easier to enter the United States from Canada, Musk applied for a Canadian passport through his Canadian-born mother. While awaiting the documentation, he attended the University of Pretoria for five months; this allowed Musk to avoid mandatory service in the South African military. Arriving in Canada in June 1989, Musk failed to locate his great-uncle in Montreal and instead stayed at a youth hostel. He then traveled west to live with a second-cousin in Saskatchewan. He stayed there for a year, working odd jobs at a farm and lumber-mill. In 1990, Musk entered Queen's University in Kingston, Ontario. Two years later, he transferred to the University of Pennsylvania; he graduated in 1997 with a Bachelor of Science (BS) degree in economics from the Wharton School and a Bachelor of Arts (BA) degree in physics.
+                </p>
+                <p>
+                    In 1994, Musk held two internships in Silicon Valley during the summer: at an energy storage start-up called Pinnacle Research Institute, which researched electrolytic ultracapacitors for energy storage, and at the Palo Alto-based start-up Rocket Science Games. Bruce Leak, the former lead engineer behind Apple's QuickTime who had hired Musk, noted: "He had boundless energy. Kids these days have no idea about hardware or how stuff works, but he had a PC hacker background and was not afraid to just go figure things out." In 1995, Musk was accepted to a Ph.D. program in energy physics/materials science at Stanford University in California. Musk attempted to get a job at Netscape but never received a response to his job inquiries. He dropped out of Stanford after two days, deciding instead to join the Internet boom and launch an internet startup.
+                </p>
+            </section>
+        </div>
+
+            <h3 class="second-h3">Business Career</h3>
+            <p class="normal">Elon Musk's two biggest companies he have created and run are Tesla and SpaceX.</p>
+
+            <section class="box-elon">
+                <h4>Tesla</h4>
+                <figure class="elon-image">
+                <img class="flexible" src="images/tesla-logo.png" alt="Image failed to load.">
+                <figcaption>Tesla Logo</figcaption>
+                </figure>
+                <p>
+                   Tesla, Inc. (originally Tesla Motors) was incorporated in July 2003 by Martin Eberhard and Marc Tarpenning, who financed the company until the Series A round of funding. Both men played active roles in the company's early development prior to Musk's involvement. Musk led the Series A round of investment in 2004, joining Tesla's board of directors as its chairman. Musk took an active role within the company and oversaw Roadster product design but was not deeply involved in day-to-day business operations. Following a series of escalating conflicts in 2007 and the 2008 financial crisis, Eberhard was ousted from the firm. Musk assumed leadership of the company as CEO and product architect in 2008, positions he still holds today. A 2009 lawsuit settlement with Eberhard designated Musk as a Tesla co-founder, along with Tarpenning and two others. As of 2019, Musk is the longest tenured CEO of any automotive manufacturer globally. 
+                </p>
+                <p>
+                    Tesla first built an electric sports car, the Tesla Roadster, in 2008. With sales of about 2,500 vehicles, it was the first serial production all-electric car to use lithium-ion battery cells. Tesla began delivery of its four-door Model S sedan in 2012; a cross-over, the Model X was launched in 2015. A mass market sedan, the Model 3 was released in 2017. As of March 2020, it is the world's best-selling electric car, with more than 500,000 units delivered. A fifth vehicle, the Model Y crossover, was launched in 2020.[106] The Cybertruck, an all-electric pickup truck, was unveiled in 2019. Under Musk, Tesla has also constructed multiple lithium-ion battery and electric vehicle subassembly factories, such as Gigafactory 1 in Nevada and Gigafactory 3 in China.
+                </p>
+                <p>
+                    Since its initial public offering in 2010, Tesla stock has risen significantly; it became the most valuable carmaker in summer 2020. It entered the S&P 500 later that year.
+                </p>
+            </section>
+
+            <section class="box-elon">
+                <h4>SpaceX</h4>
+                
+                <figure class="spacex-fig">
+                <img class="flexible" src="images/spacex.png" alt="Image failed to load.">
+                <figcaption>SpaceX's first rocket launch</figcaption>
+                </figure>
+
+                <p>
+                    In 2001, Musk conceived Mars Oasis: a miniature greenhouse on Mars would grow food crops and reawaken public interest in space exploration. In October 2001, Musk traveled with a group to Moscow to buy refurbished Dnepr Intercontinental ballistic missiles (ICBMs) that could send the greenhouse payloads into space. He met with companies such as NPO Lavochkin and Kosmotras; however, Musk was seen as a novice and was even spat on by one of the Russian chief designers. The group returned to the United States empty-handed. In February 2002, the group returned to Russia to look for three ICBMs. They had another meeting with Kosmotras and were offered one rocket for $8 million, which Musk rejected. Musk instead decided to start a company that could build affordable rockets. With $100 million of his early fortune, Musk founded Space Exploration Technologies Corp., traded as SpaceX, in May 2002.
+                </p>
+                <p>
+                    After three failed launches, SpaceX succeeded in launching the Falcon 1 in 2008. It was the first private liquid-fuel rocket to reach Earth orbit. Later that year, SpaceX received a $1.6 billion Commercial Resupply Services program contract for 12 flights of its Falcon 9 rocket and Dragon spacecraft to the International Space Station, replacing the Space Shuttle after its 2011 retirement. In 2012, the Dragon vehicle berthed with the ISS, a first for a private enterprise. Working towards its goal of reusable rockets, in 2015, SpaceX successfully landed the first stage of a Falcon 9. Landings were later achieved on an autonomous spaceport drone ship, an ocean-based recovery platform. In 2018, SpaceX launched the Falcon Heavy; the inaugural mission carried a Tesla Roadster as a dummy payload. In 2017, SpaceX unveiled its next-generation launch vehicle and spacecraft system, Big Falcon Rocket (BFR), which would support all SpaceX launch service provider capabilities. In 2018, SpaceX announced a planned 2023 lunar circumnavigation mission, a private flight called #<em>dearMoon project</em>. Two years later, SpaceX launched its first manned flight, the Demo-2, becoming the first private company to place a person into orbit and dock a crewed space-craft with the ISS.
+                </p>
+                <p>
+                    SpaceX began development of the Starlink constellation of low Earth orbit satellites in 2015 to provide satellite Internet access, with the first two prototype satellites launched in February 2018. A second set of test satellites and the first large deployment of a piece of the constellation occurred in May 2019, when the first 60 operational satellites were launched. The total cost of the decade-long project to design, build, and deploy the constellation is estimated by SpaceX to be about $10 billion, including nearly $900 million in Federal Communications Commission subsidies.
+                </p>
+            </section>
+        </article>
+
+            <aside>
+                <h3 class="third-header">Elon Musk's companies</h3>
+                <figure class="table-elon">
+                <table>
+                    <thead>
+                    <tr>
+                        <th>Year</th>
+                        <th>Name</th>
+                        <th>Worth</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>1995</td>
+                        <td>Zip2</td>
+                        <td>Sold for $307 million</td>
+                    </tr>
+                    <tr>
+                        <td>1999</td>
+                        <td>X.com and PayPal</td>
+                        <td>Sold for $1.5 Billion</td>
+                    </tr>
+                    <tr>
+                        <td>2001</td>
+                        <td>SpaceX</td>
+                        <td>$74 Billion</td>
+                    </tr>
+                    <tr>
+                        <td>2003</td>
+                        <td>Tesla</td>
+                        <td>$830 Billion</td>
+                    </tr>
+                    <tr>
+                        <td>2016</td>
+                        <td>SolarCity</td>
+                        <td>$8.4 Billion</td>
+                    </tr>
+                    <tr>
+                        <td>2016</td>
+                        <td>Neuralink</td>
+                        <td>$158 Million</td>
+                    </tr>
+                    <tr>
+                        <td>2016</td>
+                        <td>The Boring Company</td>
+                        <td>$920 Million</td>
+                    </tr>
+                </tbody>
+                </table>
+
+                <figcaption>A table showing when Elon acquired/started his companies and how much they are worth today or how much they were sold for.</figcaption>
+            </figure>
+            </aside>
+
+            <h3 class="list-header">Elon Musk's Greatest Inventions</h3>
+            <p class="normal">Elon has brought a lot to the world throughout his career and has invented many incredible things.</p>
+
+        <section class="list-alt"> 
+            <section class="list1">
+            <ul>
+                <li>Tesla Motors electric car</li>
+                <li>SpaceX, and the Falcon rocket</li>
+                <li>X.com (now known as PayPal)</li>
+                <li>Hyperloop</li>
+            </ul>
+            </section>
+            <section class="list2">
+            <ul>
+                <li>Zip2 an online city guide</li>
+                <li>SolarCity, solar-powered systems</li>
+                <li>Location-specific searches</li>
+                <li>Internet telephone calls</li>
+            </ul>
+            </section>
+        </section> <!--list-alt-->
+
+        <footer class="content-footer">
+            <h3>Citations</h3>
+            <ul>
+                <li>Elon Musk. (2021, March 04). Retrieved March 04, 2021, from <a href="https://en.wikipedia.org/wiki/Elon_Musk" target="_blank">Wikipedia: Elon Musk</a></li>
+                <li>Elon Musk's greatest inventions. (n.d.). Retrieved March 04, 2021, from <a href="https://openlab.citytech.cuny.edu/gotconcept/elon-musks-greatest-inventions/" target="_blank">GotConcept: Elon Musk's Greatest Inventions</a></li>
+            </ul>
+        </footer>
+
+    </div> <!--containter-->
+
+    <?php include "inc/scripts.php"; ?>
+        
+    </main>
+
+    <footer class="page-footer">
+        <div>Nadine Eldallal - CSC 170 Webpage Design and Development</div>
+        <div>Source: <a href="http://csc170.org/ccole15/project1/" target="_blank">Elon Musk</a></div>
+    </footer>
+
+</html>
